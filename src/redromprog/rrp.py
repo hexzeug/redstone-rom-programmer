447571f649cbb8e3b0b841229b821f2d881cec6d
Siemen Zielke <77232580+hexzeug@users.noreply.github.com>
2026-04-07 19:43:41 +0200

    added dependencies and yml rom config

--- src/redromprog/rrp.py
+++ src/redromprog/rrp.py
@@ -1,3 +1,26 @@
+import argparse
+import sys
+from yaml import load, Loader
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Redstone ROM Programmer: Convert .hex to .schem")
+    parser.add_argument("--layout", default="rom.yml", help="YAML file containing the ROM layout [defaults to rom.yml]")
+    return parser.parse_args()
+
+def open_file(rom_layout_file):
+    try:
+        file = open(rom_layout_file)
+    except FileNotFoundError:
+        print(f"Error: {rom_layout_file} does not exist.", file=sys.stderr)
+    except PermissionError:
+        print(f"Error: {rom_layout_file} is not readable", file=sys.stderr)
+    except OSError as e:
+        print(f"Error: Failed opening {rom_layout_file}, {e}", file=sys.stderr)
+    else:
+        return file
+    raise SystemExit(1)
+
 def main() -> int:
-    print("Hello CLI!")
+    args = parse_args()
+    print(load(open_file(args.layout), Loader=Loader))
     return 0
